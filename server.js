56be8e2080433e657e4ad8f3d465a2412694c20a
Santeri Saraluhta <santerisaraluhta@gmail.com>
2020-05-16 18:23:30 +0300

    Add sqlite to backend

--- server.js
+++ server.js
@@ -1,32 +1,48 @@
-'use strict'
-var express = require('express')
-var app = express()
-var path = require('path');
-var bodyParser = require('body-parser');
-var cors = require('cors')
-var storage = require('node-persist');
-var PORT = process.env.PORT || 3000;
+const express = require('express')
+const app = express()
+const bodyParser = require('body-parser')
+const cors = require('cors')
+const shortid = require('shortid')
+const PORT = process.env.PORT || 3000
 
-app.use(cors());
-app.use(bodyParser.json());
+const sqlite3 = require('sqlite3')
+const sqlite = require('sqlite')
 
-var distDir = __dirname + "/dist/scratchpad/";
-console.log(distDir)
-app.use(express.static(distDir));
+app.use(cors())
+app.use(bodyParser.json())
 
-storage.init().then(() => {
-  //app.use(express.static(path.join(__dirname, 'dist')));
-  app.listen(PORT, async () => {
+app.use(express.static(__dirname + '/dist/scratchpad/'));
+
+sqlite.open({
+  filename: ':memory:',
+  driver: sqlite3.Database
+}).then(async (db) => {
+  await db.exec('CREATE TABLE IF NOT EXISTS scratchpad (id TEXT PRIMARY KEY, name TEXT)')
+
+  for(var i = 0; i < 10; i++) {
+    await db.run('INSERT INTO scratchpad(id, name) VALUES (:id, :name)', {
+      ':id': shortid.generate(),
+      ':name': 'sp'+i
+    })
+  }
+
+  app.listen(PORT, () => {
     console.log(`Server listening on port ${PORT}`)
-    await storage.setItem('test', 'hello 2')
   })
-})
 
-app.get("/api", (req, res) => {
-  storage.getItem('test').then((data) => {
-    console.log(data)
-    res.send({ data: data });
-  }).catch(() => {
-    res.send({ error: 'not found' })
-  })
-});
+  app.get('/api', (req, res) => {
+    db.all('SELECT * FROM scratchpad').then(result => {
+      res.send(result)
+    })
+  });
+
+  app.get('/api/:id', (req, res) => {
+    db.get('SELECT name FROM scratchpad WHERE id = ?', req.params.id).then(result => {
+      if(result) {
+        res.send({ name: result.name })
+      } else {
+        res.sendStatus(404);
+      }
+    })
+  });
+})
